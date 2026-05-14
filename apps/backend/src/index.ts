a09d96dc66e1fd78ff4ecc12f3b6d79b8d94c590
webermayank <mayankkumarverma4@gmail.com>
2026-05-15 03:09:30 +0530

    change the cors config

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -8,7 +8,7 @@ import apiRoutes from "./routes/v1/index.js";
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:8081", // Allow all origins
+    origin: true,
     credentials: true
 }));
 
